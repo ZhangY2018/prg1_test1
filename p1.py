26ad029edfe83cf59dbdf6c19babe2a7615386ae
ZhangY2018 <yzhang@sjnam.org>
2018-05-28 09:44:43 -0500

    update prg1_test1 to include titles

--- p1.py
+++ p1.py
@@ -1,12 +1,7 @@
-import math
 def is_square(number):
-	'''problem 1)
-	write a function that tells you if a number is a perfect square
-	return True when the variable number is a perfect square
-	return False when the variable called number is not a perfect square
-	e.g. 
-		number=16 -> True 
-		number =15 -> False
-	'''
-	return True
- 
+	for num in number:
+	if (num == int):
+		return Ture
+	else:
+		return False
+
