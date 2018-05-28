ab6384e6726da159ef2fef267aede252922ee197
ZhangY2018 <yzhang@sjnam.org>
2018-05-28 09:53:06 -0500

    update prg1_test1 to include titles

--- p2.py
+++ p2.py
@@ -1,13 +1,6 @@
 
 def is_factorion(n):
-	'''problem 2)
-	A factorion is a number that is equal to the sum of its factorials.
-	so for example, n= 145 is a factorion. 
-	1! = 1
-	4! = 4 * 3 * 2 * 1 = 24
-	5! = 5 * 4 * 3 * 2 * 1 = 120
-	1 + 24 + 120 = 145
+	for i in n:
+		if i > 0:
 
-	Write a function that returns True if n is a factorion
-	'''
 	return True
